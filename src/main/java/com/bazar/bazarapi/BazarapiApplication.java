86ffdbc8e5d3defdd3467d747d51469005aac883
NicolasVivas93 <nicolasvivas93@gmail.com>
2024-03-23 12:55:48 -0300

    Add repo with Spring project

---
+++ src/main/java/com/bazar/bazarapi/BazarapiApplication.java
@@ -0,0 +1,13 @@
+package com.bazar.bazarapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BazarapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BazarapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bazar/bazarapi/BazarapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bazar.bazarapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BazarapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
